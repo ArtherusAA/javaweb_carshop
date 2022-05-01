f11dc36ad4831224f6c25336c3c05d6f0726fd15
ArtherusAA <artherus@yandex.ru>
2022-05-02 00:32:54 +0300

    clients and cars pages added

--- src/main/java/com/artherus/carshop/controllers/CarController.java
+++ src/main/java/com/artherus/carshop/controllers/CarController.java
@@ -1,13 +1,20 @@
 package com.artherus.carshop.controllers;
 
 import com.artherus.carshop.DAO.CarDAO;
+import com.artherus.carshop.DAO.CarModelDAO;
 import com.artherus.carshop.DAO.impl.CarDAOImpl;
+import com.artherus.carshop.DAO.impl.CarModelDAOImpl;
 import com.artherus.carshop.models.Car;
+import com.artherus.carshop.models.CarModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Controller
@@ -16,11 +23,80 @@ public class CarController {
     @Autowired
     private final CarDAO carDAO = new CarDAOImpl();
 
+    @Autowired
+    private final CarModelDAO modelDAO = new CarModelDAOImpl();
+
+    @GetMapping("/addCar")
+    public String addCarPage(Model model) {
+        model.addAttribute("modelDAO", modelDAO);
+        return "addCar";
+    }
+
     @GetMapping("/cars")
     public String carListPage(Model model) {
         List<Car> cars = (List<Car>)carDAO.getAll();
         model.addAttribute("cars", cars);
         model.addAttribute("carDAO", carDAO);
         return "cars";
+    }
+
+    @GetMapping("/car")
+    public String carPage(@RequestParam(name = "carId") Integer carId, Model model) {
+        Car car = carDAO.getCarById(carId);
+        if (car == null) {
+            model.addAttribute("error_message", String.format("Машины с номером %d нет в салоне", carId));
+            return "errorPage";
+        }
+        model.addAttribute("car", car);
+        model.addAttribute("carDAO", carDAO);
+        return "car";
+    }
+
+    @PostMapping("/createCar")
+    public String createCar(@RequestParam(name = "carModelStr") String carModelStr,
+                            @RequestParam(name = "carColor") String color,
+                            @RequestParam(name = "price") Integer price,
+                            Model model) {
+        List<CarModel> carModel = modelDAO.getCarModelsByName(carModelStr);
+        if (carModel.size() == 0) {
+            model.addAttribute("error_message", String.format("Модель %s отсутствует базе", carModelStr));
+            return "errorPage";
+        }
+        carDAO.addEntity(new Car(null, carModel.get(0), "", "", color, "",
+                        new Timestamp(0), 0, new BigDecimal(price)));
+        return "redirect:/cars";
+    }
+
+    @PostMapping("/saveCar")
+    public String saveCar(@RequestParam(name = "regId") Integer regId,
+                          @RequestParam(name = "carModel") String carModelStr,
+                          @RequestParam(name = "carColor") String color,
+                          Model model) {
+        Car car = carDAO.getCarById(regId);
+        if (car == null) {
+            model.addAttribute("error_message", String.format("Машины с номером %d нет в салоне", regId));
+            return "errorPage";
+        }
+        car.setColor(color);
+        List<CarModel> carModel = modelDAO.getCarModelsByName(carModelStr);
+        if (carModel.size() == 0) {
+            model.addAttribute("error_message", String.format("Нет модели %s", carModelStr));
+            return "errorPage";
+        }
+        car.setModel_id(carModel.get(0));
+        carDAO.updateEntity(car);
+        return String.format("redirect:/car?carId=%d", regId);
+    }
+
+    @PostMapping("/deleteCar")
+    public String deleteCar(@RequestParam(name = "regId") Integer regId,
+                            Model model) {
+        Car car = carDAO.getCarById(regId);
+        if (car == null) {
+            model.addAttribute("error_message", String.format("Машины с номером %d нет в салоне", regId));
+            return "errorPage";
+        }
+        carDAO.deleteEntity(car);
+        return "redirect:/cars";
     }
 }
---
+++ src/main/java/com/artherus/carshop/controllers/ClientController.java
@@ -0,0 +1,98 @@
+package com.artherus.carshop.controllers;
+
+import com.artherus.carshop.DAO.ClientDAO;
+import com.artherus.carshop.DAO.impl.ClientDAOImpl;
+import com.artherus.carshop.models.Car;
+import com.artherus.carshop.models.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class ClientController {
+
+    @Autowired
+    private final ClientDAO clientDAO = new ClientDAOImpl();
+
+    @GetMapping("/clients")
+    public String clientsListPage(Model model) {
+        List<Client> clients = (List<Client>)clientDAO.getAll();
+        model.addAttribute("clients", clients);
+        model.addAttribute("clientDAO", clientDAO);
+        return "clients";
+    }
+
+    @GetMapping("/client")
+    public String clientPage(@RequestParam(name = "clientId") Integer clientId, Model model) {
+        Client client = clientDAO.getClientById(clientId);
+        if (client == null) {
+            model.addAttribute(
+                    "error_message",
+                    String.format("Клиента с номером %d нет в салоне", clientId));
+            return "errorPage";
+        }
+        model.addAttribute("client", client);
+        model.addAttribute("clientDAO", clientDAO);
+        return "client";
+    }
+
+    @PostMapping("/saveClient")
+    public String saveClient(
+            @RequestParam(name = "clientId") Integer clientId,
+            @RequestParam(name = "clientName") String clientName,
+            @RequestParam(name = "clientAddress") String clientAddress,
+            @RequestParam(name = "clientPhone") String clientPhone,
+            @RequestParam(name = "clientEmail") String clientEmail,
+            Model model
+    ) {
+        Client client = clientDAO.getClientById(clientId);
+        if (client == null) {
+            model.addAttribute(
+                    "error_message",
+                    String.format("Клиента с номером %d нет в салоне", clientId));
+            return "errorPage";
+        }
+        client.setName(clientName);
+        client.setAddress(clientAddress);
+        client.setPhone(clientPhone);
+        client.setEmail(clientEmail);
+        clientDAO.updateEntity(client);
+        return String.format("redirect:/client?clientId=%d", clientId);
+    }
+
+    @PostMapping("/deleteClient")
+    public String deleteClient(@RequestParam(name = "clientId") Integer clientId, Model model) {
+        Client client = clientDAO.getClientById(clientId);
+        if (client == null) {
+            model.addAttribute(
+                    "error_message",
+                    String.format("Клиента с номером %d нет в салоне", clientId));
+            return "errorPage";
+        }
+        clientDAO.deleteEntity(client);
+        return "redirect:/clients";
+    }
+
+    @GetMapping("/addClient")
+    public String addClientPage(Model model) {
+        return "addClient";
+    }
+
+    @PostMapping("/createClient")
+    public String createClient(
+            @RequestParam(name = "clientName") String clientName,
+            @RequestParam(name = "clientAddress") String clientAddress,
+            @RequestParam(name = "clientPhone") String clientPhone,
+            @RequestParam(name = "clientEmail") String clientEmail,
+            Model model
+    ) {
+        clientDAO.addEntity(new Client(null, clientName, clientAddress, clientPhone, clientEmail));
+        return "redirect:/clients";
+    }
+
+}
